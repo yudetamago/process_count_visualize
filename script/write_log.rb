4485431f79003d23bf76f17f6eb56ac98cf6c4e1
yudetamago <yudetamago.orz@gmail.com>
2015-02-22 20:51:56 +0900

    データを送信する間隔を変更

--- script/write_log.rb
+++ script/write_log.rb
@@ -4,7 +4,7 @@ include Clockwork
 DIR_NAME = './log/'
 TIME_FORMAT = '%Y_%m_%d'
 
-every(1.seconds, 'write') do
+every(5.seconds, 'write') do
   process_count = `ps aux | wc -l`.strip
   fname = DIR_NAME +  Time.now.strftime(TIME_FORMAT) + '.log'
   content = "#{process_count} \t #{Time.now.to_s}"

e2d038793859f909cbbf69deddefb5ffac709303
yudetamago <yudetamago.orz@gmail.com>
2015-02-22 21:15:01 +0900

    書き込みを毎回flushするように変更

--- script/write_log.rb
+++ script/write_log.rb
@@ -8,5 +8,7 @@ every(5.seconds, 'write') do
   process_count = `ps aux | wc -l`.strip
   fname = DIR_NAME +  Time.now.strftime(TIME_FORMAT) + '.log'
   content = "#{process_count} \t #{Time.now.to_s}"
-  open(fname, 'a').puts(content)
+  f = open(fname, 'a')
+  f.puts(content)
+  f.close
 end

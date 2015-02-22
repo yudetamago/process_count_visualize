cc4fe9a0c304814651c8a91587ee57bfe760e19e
yudetamago <yudetamago.orz@gmail.com>
2015-02-23 00:22:30 +0900

    typo、不要なスペースの修正と削除

--- script/write_log.rb
+++ script/write_log.rb
@@ -7,7 +7,7 @@ TIME_FORMAT = '%Y_%m_%d'
 every(5.seconds, 'write') do
   process_count = `ps aux | wc -l`.strip
   fname = DIR_NAME +  Time.now.strftime(TIME_FORMAT) + '.log'
-  content = "#{process_count} \t #{Time.now.to_s}"
+  content = "#{process_count}\t#{Time.now.to_s}"
   f = open(fname, 'a')
   f.puts(content)
   f.close

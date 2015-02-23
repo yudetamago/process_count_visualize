c64678e69c9098ebc3aa7be552ca2c9582849d02
yudetamago <yudetamago.orz@gmail.com>
2015-02-23 21:22:46 +0900

    ログ書き込みをrakeに変更

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,13 +1,12 @@
-require 'clockwork'
-include Clockwork
+desc '現在のプロセス数と現在時刻をログに書き込む'
+task :write_log do
+  DIR_NAME = './log/'
+  TIME_FORMAT = '%Y_%m_%d'
 
-DIR_NAME = './log/'
-TIME_FORMAT = '%Y_%m_%d'
-
-every(5.seconds, 'write') do
   process_count = `ps aux | wc -l`.strip
   fname = DIR_NAME +  Time.now.strftime(TIME_FORMAT) + '.log'
   content = "#{process_count}\t#{Time.now.to_s}"
+  FileUtils.mkdir(DIR_NAME) if !File.exist?(DIR_NAME)
   f = open(fname, 'a')
   f.puts(content)
   f.close
